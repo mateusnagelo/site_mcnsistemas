f30b3aed495db0c5987eea12edeb01ad294f5e65
mateusnagelo <mateusgenius1234@hotmail.com>
2023-03-02 13:16:16 -0300

    correção no botão topo

--- scripts/topo.js
+++ scripts/topo.js
@@ -1,19 +1,46 @@
 // FUNCAO PARA QUE A BARRA DE ROLAGEM VOLTE PARA A POSICAO 0 NO EIXO Y.
 
-function scrollBar() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-    });
-}
+// function scrollBar() {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth',
+//     });
+// }
+
+
+
+// function controlarBotao() {
+//     if (window.scrollY === 0) {
+//         document.querySelector("#back-to-top").style.display = 'none';
+//     } else {
+//         document.querySelector("#back-to-top").style.display = 'block';
+//     }
+// }
+// window.addEventListener("scroll", controlarBotao);
+
 
 // FUNCAO QUE PARA FAZER COM QUE O BOTAO SUMA QUANDO A POSICAO Y DO SCROLL ESTIVER EM 0
 
-function controlarBotao() {
+function scrollBar() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+  
+  function controlarBotao() {
     if (window.scrollY === 0) {
-        document.querySelector("#back-to-top").style.display = 'none';
+      const botaoVoltarTopo = document.querySelector("#back-to-top");
+      if (botaoVoltarTopo) {
+        botaoVoltarTopo.style.display = 'none';
+      }
     } else {
-        document.querySelector("#back-to-top").style.display = 'block';
+      const botaoVoltarTopo = document.querySelector("#back-to-top");
+      if (botaoVoltarTopo) {
+        botaoVoltarTopo.style.display = 'block';
+      }
     }
-}
-window.addEventListener("scroll", controlarBotao);
+  }
+  
+  window.addEventListener("scroll", controlarBotao);
+  
